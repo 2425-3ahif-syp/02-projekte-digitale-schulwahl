3e02764bfc7cde85d6c3ce1c4c03892df6b405af
Antonio Subašić <antonio.subasic.public@gmail.com>
2025-06-08 17:20:37 +0200

    fix: adjust main view setup to include toast notifications

--- javafx/src/main/java/at/htl/digitaleschulwahl/app/PdfApplication.java
+++ javafx/src/main/java/at/htl/digitaleschulwahl/app/PdfApplication.java
@@ -19,19 +19,19 @@ public class PdfApplication extends Application {
             var votingController = new VotingPresenter();
 
             var mainView = new PdfView(mainController);
+            mainView.setPrimaryStage(primaryStage);
             var votingView = new VotingView(votingController);
             String css = getClass().getResource("/votingPageStyle.css").toExternalForm();
 
             var mainScene = new Scene(mainView.getRoot(), 900, 600);
             mainScene.getStylesheets().add(css);
 
-
-            /* Die scene wird erst durch einen button-click oder so angezeigt.
-               Eine zweite votingScene wird dann noch benötigt, weil einmal für SV und
-               einmal für Abteilungssprecher ... wird dann noch gehandelt!
+            /*
+             * Die scene wird erst durch einen button-click oder so angezeigt.
+             * Eine zweite votingScene wird dann noch benötigt, weil einmal für SV und
+             * einmal für Abteilungssprecher ... wird dann noch gehandelt!
              */
-            var votingScene = new Scene(votingView.getRoot(),900,600);
-
+            var votingScene = new Scene(votingView.getRoot(), 900, 600);
 
             primaryStage.setTitle("Digitale Schulwahl");
             primaryStage.setScene(mainScene);
